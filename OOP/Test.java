8ed9a152e041e5e9eb3f4afce10c15eb7be9d1a8
Mohammad anas khan <72510634+muhammedanaskhan@users.noreply.github.com>
2021-12-11 12:00:26 +0530

    Add files via upload

---
+++ OOP/Test.java
@@ -0,0 +1,25 @@
+package oops;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Student anas = new Student(13 , "khan", 19);
+		
+		
+		System.out.println(anas);
+		
+	}
+	
+	
+}
+
+ class Student {
+	
+	 int rollno;
+	 String name;
+	 int age;
+	 
+}
+
